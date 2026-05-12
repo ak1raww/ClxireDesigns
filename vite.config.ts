c1b0e55950cbf64ebe3165f5984218094d507330
ak1raww <ruinedorchid@gmail.com>
2026-05-12 21:15:22 +0200

    test fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
     base: "/",
     plugins: [react(), tailwindcss()],
-    css: {
-      postcss: {
-        plugins: [
-          require('autoprefixer')({
-            // This explicitly tells Autoprefixer not to remove the -webkit- prefix for backdrop-filter
-            overrideBrowserslist: ['Safari >= 14'],
-            grid: 'autoplace', // often needed for full compatibility
-          }),
-        ],
-      },
-    },
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
-});
+  }
+})
