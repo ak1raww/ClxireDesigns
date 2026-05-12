c5cbf5e7bb7bab1f19546d39742a806c5f570bb1
ak1raww <ruinedorchid@gmail.com>
2026-05-12 20:11:41 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import Home from './pages/Home';
 import Socials from './pages/Socials';
@@ -11,22 +11,20 @@ import CustomScrollbar from './components/CustomScrollbar';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <LanguageProvider>
-        <CustomScrollbar />
-        <Routes>
-          {/* Italian Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/socials" element={<Socials />} />
-          
-          {/* English Routes */}
-          <Route path="/en" element={<Home />} />
-          <Route path="/en/socials" element={<Socials />} />
+    <LanguageProvider>
+      <CustomScrollbar />
+      <Routes>
+        {/* Italian Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/socials" element={<Socials />} />
 
-          {/* Catch all */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </LanguageProvider>
-    </BrowserRouter>
+        {/* English Routes */}
+        <Route path="/en" element={<Home />} />
+        <Route path="/en/socials" element={<Socials />} />
+
+        {/* Catch all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </LanguageProvider>
   );
 }
